fcdfc3ee222651e9e0afafe05d8cf1d7345d2706
clover1green <110781147+clover1green@users.noreply.github.com>
2022-08-07 23:48:23 +0700

    Add files via upload

---
+++ tentang.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require 'config.php';
+$judul = 'Tentang Kami';
+
+require 'templates/header.php';
+?>
+
+<div class="row bg-white border p-2 mt-5">
+    <div class="col-md ml-3">
+        <h1>Tentang Kami</h1>
+        <p class="lead">Kami menjual berbagai jenis makanan hewan untuk kucing, anjing dan lain-lain.
+            Kami mulai membuka toko kami pada tahun 2019 bulan Januari dimana saata itu kami hanya menjual 1 jenis produk yaitu Whiskas
+            dan di pertengahan tahun 2020 kami menambahkan menambah berbagai jenis makanan.
+        </p>
+        <hr>
+        <div class="row ml-1">
+            <div class="w-100">
+                <h3><i class="fa fa-map-marker"></i>Alamat</h3>
+                <p>Bogor / Kabupaten Bogor<br>Jawa Barat<br>Indonesia<br><strong>Harman Muhammad Satrio R</strong></p>
+            <!-- /.col-sm-4-->
+            <div class="">
+                <h3><i class="fa fa-phone"></i> Hubungi Kami</h3>
+                <p class="text-muted">Anda dapat menghubungi kami dinomer yang tertera dibawah ini</p>
+                <p><strong>+681315309983</strong></p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require 'templates/footer.php' ?>
